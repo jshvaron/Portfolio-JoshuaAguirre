3ddfb7e855a420eedf78d16f7c29f749ab4fd434
jshvaron <jshva@icloud.com>
2023-05-24 18:27:42 -0500

    test

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Navbar from "./Navigation/Navbar";
+// import Navbar from "./Navigation/Navbar";
 import AboutMe from "./About";
-import Portfolio from "./Portfolio";
-import Contact from "./contact";
-import Resume from "./Resume";
-import Footer from "./Navigation/Footer";
+// import Portfolio from "./Portfolio";
+// import Contact from "./contact";
+// import Resume from "./Resume";
+// import Footer from "./Navigation/Footer";
 import '../index.css'
 
 function Main() {
